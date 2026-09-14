f62204ea4367797adf69ec7ff3e05ad0bce92e90
Sup3rone <48138402+Sup3rone@users.noreply.github.com>
2026-09-14 11:13:20 -0600

    outputFileTracingRoot + includes para empaquetar engine de Prisma en la lambda

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  outputFileTracingRoot: '../../',
+  outputFileTracingIncludes: {
+    '/**': ['./../../packages/db/src/generated/**/*']
+  }
+};
 
 const withNextIntl = createNextIntlPlugin();
 
